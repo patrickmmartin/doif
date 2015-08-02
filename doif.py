d984a4d2286c8537acbe625313a908827f827821
Patrick Martin <patrickmmartin@gmail.com>
2015-08-02 21:45:27 +0100

    updating with main runner

--- doif.py
+++ doif.py
@@ -1,39 +1,37 @@
 
-import subprocess
+def doif():
+    import subprocess
 
-# TODO(PMM) convert to reading build action from env.
-
-# TODO(PMM) read in make command
-MAKE='make.bat'
-# TODO(PMM) read in test command
-TEST='test'
-# TODO(PMM) read in targets
-targets=['pass1', 'fail1', 'pass2', 'fail2'] 
-# TODO(PMM) read in fix command
-FIX='fix2'
+    # TODO(PMM) read in make command
+    MAKE='make.bat'
+    # TODO(PMM) read in test command
+    TEST='test'
+    # TODO(PMM) read in targets
+    targets=['pass1', 'fail1', 'pass2', 'fail2'] 
+    # TODO(PMM) read in fix command
+    FIX='fix2'
 
 
-# TODO(PMM)
-for target in targets:
-    print "target %s ========================" % target
-    ret = subprocess.call([MAKE, target])
-    if (ret > 0):
-        print "target %s failed test - fixing" % target
-        ret = subprocess.call([MAKE, 'fix1'])
-        
+    # TODO(PMM)
+    for target in targets:
+        print "target %s ========================" % target
+        ret = subprocess.call([MAKE, target])
         if (ret > 0):
-            print "target %s fix process failed" % target
-            # GAME OVER
-        else:
-            ret = subprocess.call([MAKE, target])
+            print "target %s failed test - fixing" % target
+            ret = subprocess.call([MAKE, 'fix1'])
+
             if (ret > 0):
-                print "target %s failed to fix" % target
+                print "target %s fix process failed" % target
+                # GAME OVER
             else:
-                print "target %s fixed after initial fail" % target
-    else:
-        print "target %s passed test" % target
+                ret = subprocess.call([MAKE, target])
+                if (ret > 0):
+                    print "target %s failed to fix" % target
+                else:
+                    print "target %s fixed after initial fail" % target
+        else:
+            print "target %s passed test" % target
 
-
-        
-
-
+if (__name__ == '__main__'):
+    # need args: test action, fix action, target names
+    doif()
